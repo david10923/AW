21a17601341de31b602cd9a6869dc42181d3498c
David <davidf14@ucm.es>
2020-12-10 18:19:04 +0100

    Cambios en la estructura de carpetas

--- practicas/obligatorias/index.js
+++ practicas/obligatorias/index.js
@@ -9,19 +9,21 @@ const path = require('path');
 const morgan = require('morgan');
 const DaoUsers = require('./public/js/DaoUsers.js');
 const DaoQuestionAndAnswer= require('./public/js/DaoQuestionAndAnswer');
-
-
-
+const ficherosEstaticos =path.join(__dirname,"public");
 
 //const router = require('router');
 
 const app = express();
-
-// REVISAR
-// express.static("./public");
-app.use(morgan('dev'));
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "./public/views"));
+app.set("views", path.join(__dirname, "./views"));
+
+
+
+//MIDDLEWARES
+
+app.use(express.static(ficherosEstaticos));
+app.use(morgan('dev'));
+
 
 
 const pool = mysql.createPool({
@@ -50,16 +52,16 @@ app.get("/users", (request, response) => {
             response.end("Error al get users");
         } else{
             // llamar a la funcion que te devuelve los usuarios de la base de datos
-            response.sendFile("./public/css/utils.css");
+            //response.sendFile("./public/css/utils.css");
             response.render("users", { users: allUsers });
         }
     });
 });
 
-app.get("*", (request,response)=>{
-    response.type("text/plain ,charset=utf-8");
-    response.end("No hay nadaaaa");
-});
+// app.get("*", (request,response)=>{
+//     response.type("text/plain ,charset=utf-8");
+//     response.end("No hay nadaaaa");
+// });
 
 
 app.listen(3000, function(err) {
