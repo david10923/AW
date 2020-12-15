79d3c3fef47c77843196cbe5eda0cba211b583c9
csegundo <carlossn20165@gmail.com>
2020-12-15 19:48:04 +0100

    Register funcional

--- practicas/obligatorias/routers/routerLogin.js
+++ practicas/obligatorias/routers/routerLogin.js
@@ -1,33 +1,48 @@
 const express = require('express');
-const usersRouter = express.Router();
+const loginRouter = express.Router();
+const path = require('path');
 const pool = require("../database");
 const DAOUsers = require('../models/modelUsuarios');
+const bodyParser        = require('body-parser');
+let daoUsers = new DAOUsers(pool);
 
-let daoUsers = new DAOUsers(pool); 
+// middleware
+loginRouter.use(bodyParser.urlencoded({ extended: false }));
 
-usersRouter.post("/registro", (request, response) => {
-    daoUsers.createUser({email:"info2@ucm.es",password:"123",username:"Casa de estudiantes 2",profileImg:"resources/images/default.png"}, function(error){
-        if(error){
-            response.status(400);
-            response.render("error");
-        } else{
-            response.status(200);
-            response.redirect("/login");
-        }
-    });
+loginRouter.get("/registro", (request, response) => {
+    response.status(200);
+    response.sendFile(path.join(__dirname, '../public/register.html'));
 });
 
-usersRouter.post("/login", (request, response) => {
-    daoUsers.readUser("csegundo@ucm.es", function(error){
-        if(error){
-            response.status(500);
-            response.render("error");
-        } else{
-            response.status(200);
-            response.redirect("/");
-        }
-    });
+loginRouter.get("/login", (request, response) => {
+    response.status(200);
+    response.sendFile(path.join(__dirname, '../public/login.html'));
+});
+
+loginRouter.post("/registrarUsuario", function (request, response) {
+    let data = {
+        email       :request.body.email,
+        username    :request.body.username,
+        password    :request.body.password,
+        password_c  :request.body.password_confirm,
+        profileImg  :request.body.img
+    };
+
+    if(data.password === data.password_c){
+        daoUsers.createUser(data, function (error) {
+            if (error) {
+                response.status(400);
+                response.render("error");
+            } else {
+                response.status(200);
+                response.redirect("/loginout/login");
+            }
+        });
+    } else{
+        response.status(200);
+        response.redirect("/loginout/registro");
+    }
 });
 
 
-module.exports = usersRouter;
+module.exports = loginRouter;
